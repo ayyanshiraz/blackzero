aaff11ef4b47190427acd3d47bbcfea453e21108
YASIR IRSHAD <yasir.blackzero@gmail.com>
2026-06-09 11:44:41 +0500

    update

--- src/app/services/advanced-websites/page.tsx
+++ src/app/services/advanced-websites/page.tsx
@@ -37,14 +37,14 @@ const advancedWebsitesData = {
 
 export const metadata: Metadata = {
   title: `Advanced Web, App & Portal Development | Black Zero`,
-  description: `Custom Websites, 3D Experiences and Scalable Portals. Transform your digital presence with our custom web application, mobile app and Shopify development services.`,
+  description: `CustDiscover how Black Zero transforms your digital presence in the UAE. Explore our custom web apps, scalable portals, 3D experiences, and Shopify development.`,
   keywords: [`web development`, `app development`, `Next.js developers`, `mobile apps`, `UI/UX design`, `custom portals`, `3D digital platforms`],
   alternates: {
     canonical: `/services/advanced-websites`,
   },
   openGraph: {
     title: `Advanced Web, App & Portal Development | Black Zero`,
-    description: `Custom Websites, 3D Experiences and Scalable Portals. Transform your digital presence with our custom web application, mobile app and Shopify development services.`,
+    description: `CustDiscover how Black Zero transforms your digital presence in the UAE. Explore our custom web apps, scalable portals, 3D experiences, and Shopify development.`,
     url: `/services/advanced-websites`,
     siteName: `Black Zero`,
     locale: `en_US`,
