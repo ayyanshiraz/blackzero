ecb6be57290477eb75fe99ab921bf8b483a9d137
YASIR IRSHAD <yasir.blackzero@gmail.com>
2026-06-15 16:28:27 +0500

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -66,7 +66,7 @@ export default function Navbar() {
         { name: `Advanced Website and Portal Systems`, href: `/services/advanced-websites` },
         { name: `Hybrid SEO Services`, href: `/services/hybrid-seo` },
         { name: `Marketing`, href: `/services/marketing` },
-        { name: `Creative Design and Animation Studio`, href: `/services/creative-design` },
+        { name: `Creative Design and Animation Studio`, href: `/services/creative-design-and-animation-studio` },
     ];
 
     const aboutItems = [
